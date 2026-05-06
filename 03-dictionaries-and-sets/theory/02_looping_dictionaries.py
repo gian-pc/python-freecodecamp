606c6a8492b520cdafa86e78810f3c24a7665e60
gianpc <gpaucarcortez@gmail.com>
2026-05-05 21:26:51 -0500

    feat: completar teoría 02 - iterar diccionarios

--- 03-dictionaries-and-sets/theory/02_looping_dictionaries.py
+++ 03-dictionaries-and-sets/theory/02_looping_dictionaries.py
@@ -6,5 +6,45 @@ products = {
     'Headphones': 70,
 }
 
+# iterando sobre los valores
 for price in products.values():
     print(price)
+
+# iterando sobre los keys
+for product in products.keys():
+    print(product)
+
+# iterando sobre keys y valores
+for product in products.items():
+    print(product)
+
+# almacenando las keys y valores en variables separadas
+for product, price in products.items():
+    print(product, price)
+
+# aplicando un descuento del 20% al precio del producto
+for product, price in products.items():
+    products[product] = round(price * 0.8)
+
+print(products)
+
+# --------------  enumerate  -----------------
+# la función enumerate asigna un número a cada par clave valor
+for product in enumerate(products):
+    print(product)
+
+# también se puede asignar valores a variables separadas
+for index, product in enumerate(products):
+    print(index, product)
+
+# iterando sobre los valores
+for price in enumerate(products.values()):
+    print(price)
+
+# también puedes asignarle a variables separadas
+for index, price in enumerate(products.values()):
+    print(index, price)
+
+# ahora con items()
+for index, product in enumerate(products.items()):
+    print(index, product)
